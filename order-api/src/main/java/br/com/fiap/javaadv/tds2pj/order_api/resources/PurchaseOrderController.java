e5b9a36989c8428817faa9466a630a73f272c0c1
Orlando <orlando.patriarcha@gmail.com>
2026-03-16 11:55:07 -0300

    criacao inicial do resource de PurchaseOrders

--- order-api/src/main/java/br/com/fiap/javaadv/tds2pj/order_api/resources/PurchaseOrderController.java
+++ order-api/src/main/java/br/com/fiap/javaadv/tds2pj/order_api/resources/PurchaseOrderController.java
@@ -1,13 +1,17 @@
 package br.com.fiap.javaadv.tds2pj.order_api.resources;
 
+import br.com.fiap.javaadv.tds2pj.order_api.domainmodel.entity.OrderStatus;
 import br.com.fiap.javaadv.tds2pj.order_api.domainmodel.entity.PurchaseOrder;
 import br.com.fiap.javaadv.tds2pj.order_api.service.PurchaseOrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -21,6 +25,39 @@ public class PurchaseOrderController {
         PurchaseOrder response = this.orderService.create(purchaseOrder);
 
         return ResponseEntity.ok(response);
+    }
+
+    @PutMapping   //PUT http://localhost:8080/api/orders
+    public ResponseEntity<PurchaseOrder> update(@RequestBody PurchaseOrder purchaseOrder){
+        return ResponseEntity.ok(this.orderService.update(purchaseOrder));
+    }
+
+    @DeleteMapping ("/{id}")//DELETE http://localhost:8080/api/orders/{id}
+    public ResponseEntity<Void> delete( @PathVariable Long id){
+        this.orderService.remove(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}") // GET http://localhost:8080/api/orders/{id}
+    public ResponseEntity<PurchaseOrder>  findById( @PathVariable Long id){
+        return ResponseEntity.ok(this.orderService.findById(id));
+    }
+
+    @GetMapping  // GET http://localhost:8080/api/orders
+    public ResponseEntity<Page<PurchaseOrder>> findAll(
+            @PageableDefault(page=0, size=10, sort = "id")
+            Pageable pageable){
+        return ResponseEntity.ok(this.orderService.findAllPaged( pageable));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<PurchaseOrder>> search(
+            @RequestParam( required = true) String customerName,
+            @RequestParam( required = true ) OrderStatus status,
+            @RequestParam( required = true ) BigDecimal minTotal,
+            @PageableDefault( page = 0, size=10, sort = "id" ) Pageable pageable
+            ){
+        return ResponseEntity.ok(this.orderService.search(customerName, status, minTotal, pageable));
     }
 
 }
--- order-api/src/main/java/br/com/fiap/javaadv/tds2pj/order_api/service/PurchaseOrderService.java
+++ order-api/src/main/java/br/com/fiap/javaadv/tds2pj/order_api/service/PurchaseOrderService.java
@@ -1,12 +1,43 @@
 package br.com.fiap.javaadv.tds2pj.order_api.service;
 
+import br.com.fiap.javaadv.tds2pj.order_api.domainmodel.entity.OrderStatus;
 import br.com.fiap.javaadv.tds2pj.order_api.domainmodel.entity.PurchaseOrder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class PurchaseOrderService {
     public PurchaseOrder create(PurchaseOrder purchaseOrder) {
         System.out.println("FAZ DE CONTA QUE CRIOU SAPOHA");
+        purchaseOrder.setId(1L);
         return purchaseOrder;
+    }
+
+    public PurchaseOrder update(PurchaseOrder purchaseOrder) {
+        System.out.println("FAZ DE CONTA QUE CRIOU SAPOHA");
+        return purchaseOrder;
+    }
+
+    public void remove(Long id) {
+        System.out.println("FAZ DE CONTA QUE DELETOU SAPOHA");
+    }
+
+    public PurchaseOrder findById(Long id) {
+
+        return new PurchaseOrder(id, null, null, null, null);
+    }
+
+    public Page findAllPaged(Pageable pageable) {
+        return null;
+    }
+
+    public Page<PurchaseOrder> search(String customerName, OrderStatus status, BigDecimal minTotal, Pageable pageable) {
+        return null;
     }
 }
