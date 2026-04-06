4e7881b959d405864abe53b75fac03a36c56428a
Orlando <orlando.patriarcha@gmail.com>
2026-04-06 11:53:44 -0300

    adicao do querydsl

---
+++ backend/src/main/java/br/com/fiap/javaadv/blog/backend/datasource/repositories/OrderItemRepository.java
@@ -0,0 +1,12 @@
+package br.com.fiap.javaadv.blog.backend.datasource.repositories;
+
+import br.com.fiap.javaadv.blog.backend.domainmodel.entities.OrderItem;
+import br.com.fiap.javaadv.blog.backend.domainmodel.entities.OrderItemKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface OrderItemRepository extends
+        JpaRepository<OrderItem, OrderItemKey>,
+        QuerydslPredicateExecutor<OrderItem>,
+        OrderRepositoryCustom{
+}
