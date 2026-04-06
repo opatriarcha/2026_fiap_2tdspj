64375586606d54e351f6ad85d476460a160f791b
Orlando <orlando.patriarcha@gmail.com>
2026-04-06 12:00:20 -0300

    adiccao d aimplementacao fisica do OrderItemRepositoryCustom

---
+++ backend/src/main/java/br/com/fiap/javaadv/blog/backend/datasource/repositories/OrderItemRepositoryCustomImpl.java
@@ -0,0 +1,7 @@
+package br.com.fiap.javaadv.blog.backend.datasource.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderItemRepositoryCustomImpl implements OrderRepositoryCustom{
+}
